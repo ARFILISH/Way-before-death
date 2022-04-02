23d564f57c34087cf3a79dde9df13ebd353cc65c
Arseniy Filyushkin <cenya-filyushkin@outlook.com>
2022-04-03 01:58:11 +0300

    Added additional level scripting classes.

---
+++ Assets/_Scripts/EventQueue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace _Scripts
+{
+    public class EventQueue : MonoBehaviour
+    {
+        public List<UnityEvent> eventsToCall;
+
+        [SerializeField] private int _currentQueuePlace = 0;
+
+        public void CallQueuePlace(int place)
+        {
+            _currentQueuePlace = Mathf.Min(place, eventsToCall.Count - 1);
+            eventsToCall[_currentQueuePlace].Invoke();
+        }
+
+        public void AddQueuePlace()
+        {
+            CallQueuePlace(_currentQueuePlace + 1);
+        }
+        
+        public void SubstractQueuePlace()
+        {
+            CallQueuePlace(_currentQueuePlace - 1);
+        }
+    }
+}
--- Assets/_Scripts/Gate.cs
+++ Assets/_Scripts/Gate.cs
@@ -1,8 +1,65 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace _Scripts
 {
     public class Gate : MonoBehaviour
     {
+        [SerializeField] private int _numberToOpen = 2;
+        [SerializeField] private bool _opened = false;
+        
+        public UnityEvent gateOpened;
+        public UnityEvent gateClosed;
+
+        private int _currentNumber = 0;
+
+        private void Start()
+        {
+            if (_opened)
+            {
+                SetGateNumber(_numberToOpen);
+            }
+        }
+        
+        public void OpenGate()
+        {
+            _opened = true;
+            gateOpened.Invoke();
+        }
+
+        public void CloseGate()
+        {
+            _opened = false;
+            gateClosed.Invoke();
+        }
+
+        public void SetGateNumber(int number)
+        {
+            _currentNumber = Mathf.Min(number, _numberToOpen);
+            if (_currentNumber == _numberToOpen && !_opened)
+            {
+                OpenGate();
+            }
+            else if (_currentNumber != _numberToOpen && _opened)
+            {
+                CloseGate();
+            }
+        }
+
+        public void AddGateNumber()
+        {
+            SetGateNumber(_currentNumber + 1);
+        }
+
+        public void SubstractGateNumber()
+        {
+            SetGateNumber(_currentNumber - 1);
+        }
+
+        public void ResetGate()
+        {
+            SetGateNumber(0);
+        }
     }
 }
